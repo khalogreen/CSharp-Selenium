baa5814b66e6bdf932036c825da17fc23b57ed12
Khalid Chumaev <KChumaev@actsoft.com>
2025-10-31 22:42:42 +0600

    Add project files.

---
+++ C# + Selenium/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace C____Selenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
